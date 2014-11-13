4cb892877a6b394a0137be18460d225decebc748
Pascal Hartig <passy@twitter.com>
2014-11-13 23:09:28 +0000

    Hello, World.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 'use scrict';
 
 var gulp = require('gulp');
+var del = require('del');
 var purescript = require('gulp-purescript');
 
 gulp.task('build', function () {
     gulp.src('purs/*.purs')
         .pipe(purescript.psc({
-            output: 'js/app.js'
+            output: 'app.js',
+            main: true
         }))
         .pipe(gulp.dest('js/'));
 });
 
+gulp.task('clean', del.bind(null, 'js'));
+
 gulp.task('watch', function () {
     gulp.watch('purs/*.purs', ['build']);
-})
+});
 
-gulp.task('default', ['build']);
+gulp.task('default', ['clean', 'build']);
---
+++ purs/main.purs
@@ -0,0 +1,6 @@
+module Main where
+
+import Debug.Trace (trace)
+
+main = do
+    trace "Hello, World."
