568d5e0840b65109bc69d9be639b075f19a80bcd
Pascal Hartig <passy@twitter.com>
2014-11-16 12:02:36 +0000

    Add header input box

--- purs/main.purs
+++ purs/main.purs
@@ -5,11 +5,23 @@ import Debug.Trace (trace)
 import React
 import React.DOM
 
+todoHeader = mkUI spec do
+  pure $ header [ idProp "header" ]
+    [ h1' [ text "todos" ]
+    , input
+      [ idProp "new-todo"
+      , placeholder "What needs to be done?"
+      , autoFocus "true"
+      ] []
+    ]
+
+helloWorld :: { what :: String } -> React.UI
 helloWorld = mkUI spec do
   props <- getProps
   pure $ h1 [className "hello"] [
       text $ "Hello, " ++ props.what
     ]
 
+main :: Eff (dom :: React.DOM) React.UI
 main = do
-  renderToElementById "todoapp" $ helloWorld {what: "Paris"}
+  renderToElementById "todoapp" $ todoHeader []
